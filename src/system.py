181b567e78e123799c2aea7b7d18ed4499260de4
Emre Ozdemir <emreozdemir@gmail.com>
2017-03-22 15:55:57 -0700

    make it work!

--- src/system.py
+++ src/system.py
@@ -8,8 +8,8 @@ class ReccomnderSystem(object):
         self.df_reviews = df_reviews
         self.df_select = None
 
-        self.reccomender_dictionary = None
-        self.result_dict = None
+        self.reccomender_dictionary={}
+
         self.listings = None
         self.alt_neig_1 = None
         self.alt_neig_2 = None
@@ -19,107 +19,95 @@ class ReccomnderSystem(object):
         self.minbath = None
         self.maxbath = None
         self.proptype = None
-#       self.neighborhood = None
+
+        self.neighborhood = None
 
         nrec = ListingReccomender(self.df_reviews)
         self.neighborhood_dict = nrec.top_three_dictionary(df_reviews['reviews'].values,
                                                       df_reviews['name'].values,
                                                       posttagged = True)
 
-    def user_select(self, minbed, maxbed, minbath, maxbath, proptype, neighborhood):
+    def input_func(self,minbed, maxbed, minbath, maxbath, proptype, neighborhood):
         self.minbed, self.maxbed = sorted([minbed,maxbed])
         self.minbath, self.maxbath = sorted([minbath,maxbath])
         self.proptype = proptype
-#        self.neighborhood = neighborhood
+        self.neighborhood = neighborhood
+        self.user_select(self.neighborhood)
+        if self.df_select.shape[0] == 0:
+            print 'Sorry, no match found. Please change your search options and try again :)'
+        else :
+            print self.df_select['id'].values.tolist()
+
+    def user_select(self, neighborhood_):
 
         self.df_select = self.df_sample[(self.df_sample['bed']<=self.maxbed) &
                                   (self.df_sample['bed']>=self.minbed) &
                                   (self.df_sample['bath']<=self.maxbath) &
                                   (self.df_sample['bed']>=self.minbath) &
                                   (self.df_sample['proptype']==self.proptype)&
-                                  (self.df_sample['street_neighborhood']==neighborhood)]\
+                                  (self.df_sample['street_neighborhood']==neighborhood_)]\
                                   [['id','remarks']]
 
         self.df_select.reset_index(drop=True,inplace=True)
 
+
+## IF ONE
+    def if_one(self):
+        self.reccomender_dictionary[self.df_select['id'].values[0]]=[]
+
+## IF TWO OR THREE
+    def if_less_than_three(self):
+        for i in range(self.df_select.shape[0]):
+            self.reccomender_dictionary[self.df_select['id'].values[i]]\
+                            = np.setdiff1d(self.df_select['id'].values,
+                              self.df_select['id'].values[i]).tolist()
+# IF THREE
+    def if_more_than_three(self,alt_,alt_id_):
+        rec=ListingReccomender(self.df_sample)
+        self.reccomender_dictionary\
+                = rec.top_three_dictionary(self.df_select['remarks'].values,
+                                           self.df_select['id'].values,
+                                           alt=alt_,
+                                           alt_id=alt_id_)
+
+
 ## PART I - RETURN LISTINGS BASED ON USER'S SPECIFICATIONS
-    def home_reccomender_dict(self, minbed, maxbed, minbath, maxbath, proptype,
-                              neighborhood, alt=False, alt_id=None):
-        ## user selection data frame df_select
-        self.user_select(minbed,maxbed,minbath,maxbath,proptype,neighborhood)
+    def same_neighborhood_reccomender(self,alt_=False, alt_id_=None):
 
-        ## check if there is any match
-        if self.df_select.shape[0]==0:
-            return 'Sorry, no match found. Please change your search options and try again :)'
+        self.user_select(self.neighborhood)
 
-        ## if there is only one return
         if self.df_select.shape[0]==1:
-            return self.df_select
-
-        ## check if there number of listing 2 or more
+            self.if_one()
         elif self.df_select.shape[0]<=3:
-            for i in range(self.df_select.shape[0]):
-                self.reccomender_dictionary[self.df_select['id'].values[i]]\
-                                = np.setdiff1d(self.df_select['id'].values,
-                                  self.df_select['id'].values[i])
-            self.listings = self.reccomender_dictionary.keys()
-            return self.listings
+            self.if_less_than_three()
+        else :
+            self.if_more_than_three(alt_,alt_id_)
 
-        else:
+    def alt_neighborhood_reccomender(self,neighborhood_,alt_=False, alt_id_=None):
 
-            self.df_select.reset_index(drop=True,inplace=True)
-
-            rec=ListingReccomender(self.df_sample)
-            self.reccomender_dictionary\
-                    = rec.top_three_dictionary(self.df_select['remarks'].values,
-                                               self.df_select['id'].values,
-                                               alt=alt,
-                                               alt_id=alt_id)
-            self.listings = self.reccomender_dictionary.keys()
-            return self.listings
+        self.user_select(neighborhood_)
+        if self.df_select.shape[0]==0:
+            self.reccomender_dictionary[alt_id_]=[]
+        elif self.df_select.shape[0]<=2:
+            self.reccomender_dictionary[alt_id_]= self.df_select['id'].values.tolist()
+        else :
+            self.if_more_than_three(alt_,alt_id_)
 
 ## PART II RETURN RECCOMENDED LISTINGS BASED ON USER'S SELECTIONS
-    def listing_reccomender(self,selected_listing,neighborhood):
-
-        _=self.home_reccomender_dict(self.minbed,
-                                   self.maxbed,
-                                   self.minbath,
-                                   self.maxbath,
-                                   self.proptype,
-                                   neighborhood)
+    def listing_reccomender(self,selected_listing):
         self.result_dict={}
-        self.selected_listing = selected_listing
+        self.alt_neig_1, self.alt_neig_2 = self.neighborhood_dict[self.neighborhood]
 
-        self.result_dict[neighborhood] = self.reccomender_dictionary[self.selected_listing]
+        for n in [self.neighborhood, self.alt_neig_1, self.alt_neig_2 ]:
+            if n == self.neighborhood :
+                self.same_neighborhood_reccomender(alt_=False, alt_id_=None)
+                if len(self.reccomender_dictionary[selected_listing])!=0:
+                    self.result_dict[n] = self.reccomender_dictionary[selected_listing]
 
-        self.alt_neig_1, self.alt_neig_2 = self.neighborhood_dict[neighborhood]
-
-        # alt_neig_1
-        _= self.home_reccomender_dict(self.minbed,
-                                      self.maxbed,
-                                      self.minbath,
-                                      self.maxbath,
-                                      self.proptype,
-                                      self.alt_neig_1,
-                                      alt=True,
-                                      alt_id=selected_listing)
-
-        self.result_dict[self.alt_neig_1] = self.reccomender_dictionary.\
-                                                    get(selected_listing,
-                                                        'sorry, no match found')
-
-        # alt_neig_2
-        _= self.home_reccomender_dict(self.minbed,
-                                      self.maxbed,
-                                      self.minbath,
-                                      self.maxbath,
-                                      self.proptype,
-                                      self.alt_neig_2,
-                                      alt=True,
-                                      alt_id=selected_listing)
-
-        self.result_dict[self.alt_neig_2] = self.reccomender_dictionary.\
-                                                    get(selected_listing,
-                                                        'sorry, no match found')
-
-        return self.result_dict
+            else:
+                self.alt_neighborhood_reccomender(neighborhood_=n,
+                                                  alt_=True,
+                                                  alt_id_=selected_listing)
+                if len(self.reccomender_dictionary[selected_listing])!=0:
+                    self.result_dict[n] = self.reccomender_dictionary[selected_listing]
+        print self.result_dict
