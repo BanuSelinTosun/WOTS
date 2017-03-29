d4828b31e5092191434f489190dd8b2a2e276e1f
Emre Ozdemir <emreozdemir@gmail.com>
2017-03-28 21:21:27 -0700

    clean up

--- src/recommender.py
+++ src/recommender.py
@@ -16,7 +16,36 @@ class StemmedTfidfVectorizer(TfidfVectorizer):
 
 class TextCleaning(object):
 
-    def _fix_abrv(self,text_):
+    def _fix_abrv(self,word_):
+        '''
+        INPUT:
+        - word_: string
+
+        OUTPUT: string
+
+        Fix the abbrivations.
+        '''
+        return (word_.replace('\\n','')
+                         .replace('bdrm', 'bedroom')
+                         .replace('br', 'bedroom')
+                         .replace('bdr', 'bedroom')
+                         .replace('BR','bedroom')
+                         .replace('ba','bathroom')
+                         .replace('BA','bathroom')
+                         .replace('Mstr','master')
+                         .replace('mstr','master')
+                         .replace('Rm','room')
+                         .replace('rm','room')
+                         .replace('Bsmnt','basement')
+                         .replace('bsmnt','basement')
+                         .replace('appls', 'appliances')
+                         .replace('appls', 'appliances')
+                         .replace('flrs', 'floors')
+                         .replace('flr', 'floor')
+                         .replace('HW','hardwood')
+                         .replace('SS','stainless steel'))
+
+    def _abbr(self,text_):
         '''
         INPUT:
         - text_: string
@@ -25,23 +54,15 @@ class TextCleaning(object):
 
         Fix the abbrivations.
         '''
-        return (text_.replace('\\n','')
-                         .replace(' ba ',' bathroom ')
-                         .replace(' bdrm ', ' bedroom ')
-                         .replace(' br ', ' bedroom ')
-                         .replace(' appls ', ' appliances ')
-                         .replace(' appls ', ' appliances ')
-                         .replace(' bdr ', ' bedroom ')
-                         .replace(' flrs ', ' floors ')
-                         .replace(' flr ', ' floor '))
+        return ' '.join(self._fix_abrv(word_) for word_ in text_.split() )
 
-    def _post_tag_nouns_adj(self,text_):
+    def _pos_tag_nouns_adj(self,text_):
         '''
         INPUT:
         - text_: string
         OUTPUT: string, adjactives and nouns only
 
-        Apply post_tag and return adjactves and nouns only
+        Apply pos_tag and return adjactves and nouns only
         '''
         return ' '.join([t[0] for t in pos_tag(text_.split())if t[1].startswith(('JJ','NN'))])
 
@@ -66,22 +87,22 @@ class TextCleaning(object):
         '''
         return ' '.join(s for s in text_.split() if not any(c.isdigit() for c in s))
 
-    def clean_text(self,text_list,post_tagged=False):
+    def clean_text(self,text_list,pos_tagged=False):
         '''
         INPUT:
         - text_list: list of numpy array strings
         OUTPUT: list of  numpy array strings
 
         Fix abbrivations, Remove punctuations,
-        If post_tagged = True apply post_tagged and return Adj and Nouns only
-        If post_tagged = False remove digits and return list of strings
+        If pos_tagged = True apply pos_tagged and return Adj and Nouns only
+        If pos_tagged = False remove digits and return list of strings
         '''
         cleaned_text_list=[]
         for i in range(len(text_list)):
-            review_fixed = self._fix_abrv(text_list[i])
+            review_fixed = self._abbr(text_list[i])
             review_no_punc = self._remove_punctuations(review_fixed)
-            if post_tagged==True:
-                cleaned_text_list.append(self._post_tag_nouns_adj(review_no_punc))
+            if pos_tagged==True:
+                cleaned_text_list.append(self._pos_tag_nouns_adj(review_no_punc))
             else:
                 cleaned_text_list.append(self._remove_digits(review_no_punc))
         return cleaned_text_list
@@ -92,7 +113,7 @@ class Listingrecommender(object):
         self.df_sample = df_sample
         self.stopwords_= set(stopwords.words('english'))
         self.seattle_stopwords = [u'1st', u'2nd', u'3rd', u'alki', u'anne',
-               u'arbor', u'area', u'atlantic', u'baker',
+               u'arbor', u'area', u'atlantic',u'admiral', u'baker',
                u'ballard', u'bay', u'beach', u'beacon',
                u'belltown', u'bitter',
                u'blaine', u'blue', u'broadview', u'broadway', u'bryant',
@@ -108,7 +129,7 @@ class Listingrecommender(object):
                u'neighborhood', u'north', u'northgate', u'olympic', u'park',
                u'phinney', u'pike', u'pioneer', u'point', u'portage', u'queen',
                u'rainier', u'ravenna', u'ridge', u'riverview', u'roxhill', u'sand',
-               u'seattleadmiral', u'seaview', u'seward', u'south', u'springs',
+               u'seattle' u'street', u'seaview', u'seward', u'south', u'springs',
                u'square', u'stevens', u'sunset', u'terrace', u'union',
                u'university', u'victory', u'wallingford', u'wedgeview', u'west',
                u'westlake', u'whittier', u'yesler']
@@ -126,8 +147,18 @@ class Listingrecommender(object):
         for word in self.seattle_stopwords:
             self.stopwords_.add(word)
 
-    def top_three_dictionary(self, text_list,label_list, posttagged=False,
+    def top_three_dictionary(self, text_list,label_list, postagged=False,
                             ngrammin=2, ngrammax=2, alt=False, alt_id=None):
+        '''
+        INPUT
+        -text_list: list of strings
+        -label_list: list of strings
+        -postagged : True if clean_text(postagged=True)
+
+        OUTPUT:
+        -similatiry_dict : dictionary of top three similar strings
+
+        '''
 
         ## check if it is an alternative neighborhood
         if alt == True:
@@ -138,7 +169,7 @@ class Listingrecommender(object):
 
         ## get cleaned textreview
         cleaner=TextCleaning()
-        textreview = cleaner.clean_text(text_list,post_tagged=posttagged)
+        textreview = cleaner.clean_text(text_list,pos_tagged=postagged)
 
         ## stemmed vectorizer
         Stemmed_Vectorizer=StemmedTfidfVectorizer(stop_words=self.stopwords_,
