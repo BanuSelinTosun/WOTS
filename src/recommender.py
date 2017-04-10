41b0d644e4c0e81411e686b72eaca9a4a7fa64a5
Emre Ozdemir <emreozdemir@gmail.com>
2017-04-10 13:24:59 -0700

    update docstrings in recommender.py

--- src/recommender.py
+++ src/recommender.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import string
-#NLP 
+#NLP
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -17,14 +17,12 @@ class StemmedTfidfVectorizer(TfidfVectorizer):
 class TextCleaning(object):
 
     def _fix_abrv(self,word_):
-        '''
-        INPUT:
-        - word_: string
+        """ Replaces the abbrivations that are commen in the real-estate text descriptions
+        ARGS:
+        word_ (string): text reviews
 
-        OUTPUT: string
-
-        Fix the abbrivations.
-        '''
+        RETURNS (string): text reviews
+        """
         return (word_.replace('\\n','')
                          .replace('bdrm', 'bedroom')
                          .replace('br', 'bedroom')
@@ -46,57 +44,53 @@ class TextCleaning(object):
                          .replace('SS','stainless steel'))
 
     def _abbr(self,text_):
-        '''
-        INPUT:
-        - text_: string
+        """ Applies the _fix_abrv function to the given the text
+        ARGS:
+        text_ (string) :  text reviews
 
-        OUTPUT: string
-
-        Fix the abbrivations.
-        '''
+        RETURNS (string) : text reviews
+        """
         return ' '.join(self._fix_abrv(word_) for word_ in text_.split() )
 
     def _pos_tag_nouns_adj(self,text_):
-        '''
-        INPUT:
-        - text_: string
-        OUTPUT: string, adjactives and nouns only
+        """ 'Part Of Speach' tagging: extracts nouns and adjectives from the given neihborhood reviews
+        ARGS:
+        text_ (string) : text reviews
 
-        Apply pos_tag and return adjactves and nouns only
-        '''
+        RETURNS (string) : text reviews
+        """
         return ' '.join([t[0] for t in pos_tag(text_.split())if t[1].startswith(('JJ','NN'))])
 
     def _remove_punctuations(self,text_):
-        '''
-        INPUT:
-        - text_: string
-        OUTPUT: string
+        """ Removes punctuations from the given text
+        ARGS:
+        text_ (string) : text reviews
 
-        Removes punctuations
-        '''
+        RETURNS (string) text reviews
+        """
         punctuations_=set(string.punctuation)
         return ''.join(word for word in text_ if word not in punctuations_)
 
     def _remove_digits(self,text_):
-        '''
-        INPUT:
-        - text_: string
-        OUTPUT: string
+        """ Removes digits from the given text
+        ARGS:
+        text_ (string) : text reviews
 
-        Removes digits
-        '''
+        RETURNS (string) : text reviews
+        """
         return ' '.join(s for s in text_.split() if not any(c.isdigit() for c in s))
 
     def clean_text(self,text_list,pos_tagged=False):
-        '''
-        INPUT:
-        - text_list: list of numpy array strings
-        OUTPUT: list of  numpy array strings
+        """ Fixes abbrivations, Removes punctuations,
+        ARGS:
+        text_list (list): list of strings
+        pos_tagged (boolean): default False
+            If pos_tagged = True apply pos_tagged and return Adj and Nouns only
+            If pos_tagged = False remove digits and return list of strings
 
-        Fix abbrivations, Remove punctuations,
-        If pos_tagged = True apply pos_tagged and return Adj and Nouns only
-        If pos_tagged = False remove digits and return list of strings
-        '''
+        RETURNS
+        cleaned_text_list (list) : list of strings
+        """
         cleaned_text_list=[]
         for i in range(len(text_list)):
             review_fixed = self._abbr(text_list[i])
@@ -110,7 +104,18 @@ class TextCleaning(object):
 
 class Listingrecommender(object):
     def __init__(self,df_sample, combine_stopwords=True):
+        """
+        ARGS:
+        df_sample (DataFrame): sample listing data frame
+        combine_stopwords (boolean) : default True, True if add custom stopwords
+
+        ATTRIBUTES:
+        Stemmed_Review_Vectors : default None, stemmed review vectors
+        stopwords : english stopwords based on nltk.corpus
+        seattle_stopwords (list) : list of custom neighborhood stopwords
+        """
         self.df_sample = df_sample
+        self.Stemmed_Review_Vectors = None
         self.stopwords_= set(stopwords.words('english'))
         self.seattle_stopwords = [u'1st', u'2nd', u'3rd', u'alki', u'anne',
                u'arbor', u'area', u'atlantic',u'admiral', u'baker',
@@ -137,28 +142,25 @@ class Listingrecommender(object):
             self._add_stopwords()
 
     def _add_stopwords (self):
-        '''
-        INPUT: None
+        """ add custom stop words.
+        ARGS: None
 
-        OUTPUT: None
-
-        add custom stop words).
-        '''
+        RETURNS: None
+        """
         for word in self.seattle_stopwords:
             self.stopwords_.add(word)
 
     def top_three_dictionary(self, text_list,label_list, postagged=False,
                             ngrammin=2, ngrammax=2, alt=False, alt_id=None):
-        '''
-        INPUT
-        -text_list: list of strings
-        -label_list: list of strings
-        -postagged : True if clean_text(postagged=True)
+        """ Compares text reviews and returns top three similar ones
+        ARGS:
+        text_list (list): list of strings
+        label_list (list): list of strings
+        pos_tagged (boolean): default False
 
-        OUTPUT:
-        -similatiry_dict : dictionary of top three similar strings
-
-        '''
+        RETURNS:
+        similatiry_dict (dict) : dictionary of top three similar strings
+        """
 
         ## check if it is an alternative neighborhood
         if alt == True:
@@ -175,13 +177,13 @@ class Listingrecommender(object):
         Stemmed_Vectorizer=StemmedTfidfVectorizer(stop_words=self.stopwords_,
                                                   ngram_range=(ngrammin, ngrammin))
         Stemmed_Vectors=Stemmed_Vectorizer.fit_transform(textreview)
-        Stemmed_Review_Vectors=Stemmed_Vectors.toarray()
+        self.Stemmed_Review_Vectors=Stemmed_Vectors.toarray()
 
         ## find 3 most similar items
         n=3
         similatiry_dict={}
         for i in range(len(label_list)):
-            cos_sim = cosine_similarity(Stemmed_Review_Vectors[i:(i+1)], Stemmed_Review_Vectors)
+            cos_sim = cosine_similarity(self.Stemmed_Review_Vectors[i:(i+1)], self.Stemmed_Review_Vectors)
             order = list(cos_sim.argsort()[0][::-1][1:n])
             top_three = label_list[order]
             similatiry_dict[label_list[i]]=top_three.tolist()
